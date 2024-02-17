d5697aec7e9628c8050c4fb579ac69891f4495e8
Will Burgess <112338817+will-brgs@users.noreply.github.com>
2024-02-17 11:56:38 -0600

    Add files via upload

---
+++ WrittenHw4.m
@@ -0,0 +1,6 @@
+%% Signals and Systems Written Homework #4
+%% Introduction
+% * Author:                   Will Burgess
+% * Class:                    ESE 351
+% * Date:                     Created 1/25/2024, Last Edited 2/06/2024
+% * With contributions from:  Mack Larosa, Tasha Igic, Mischa Tranor
